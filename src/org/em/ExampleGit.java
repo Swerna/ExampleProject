9745b716864335c0e028012f2400a05989a88529
Swerna <swerna2398@gmail.com>
2021-12-07 18:16:21 +0530

    conflict resolved

--- src/org/em/ExampleGit.java
+++ src/org/em/ExampleGit.java
@@ -2,11 +2,16 @@ package org.em;
 
 public class ExampleGit {
 
+	public static void UserName() {
+		System.out.println("UserName Functionality");
+	}
+
 	public static void passWord() {
 		System.out.println("password Functionality");
 	}
-
+	
 public static void main(String[] args) {
+	UserName();
 	passWord();
 }
 }
