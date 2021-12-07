52e76ac33759ee53ec2272505da54d69b8ef311a
Swerna <swerna2398@gmail.com>
2021-12-07 17:08:41 +0530

    Password Functionality completed

--- src/org/em/ExampleGit.java
+++ src/org/em/ExampleGit.java
@@ -2,4 +2,14 @@ package org.em;
 
 public class ExampleGit {
 
+	public static void passWord() {
+		System.out.println("password Functionality");
+	}
+
+public static void main(String[] args) {
+	passWord();
 }
+}
+
+
+
